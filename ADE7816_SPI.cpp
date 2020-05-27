7b717101a5bef5f74c8b719e75b5ba3f7a6c2946
f1andrew <60043578+f1andrew@users.noreply.github.com>
2020-05-27 16:33:51 +0300

    SPI implementation (#2)
    * SPI implementation
    
    * SPI implementation
    
    * added compatibility with esp8266; and retained class hierarchy

--- ADE7816_SPI.cpp
+++ ADE7816_SPI.cpp
@@ -3,20 +3,96 @@
 /* https://github.com/tadakado/smart-meter/blob/master/Soft/OLD_spi/02_initialize.js */
 /*************************************************************************************/
 #include "ADE7816.h"
+#include "SPI.h"
 
-ADE7816_SPI::ADE7816_SPI(int pin)
+SPIClass spi;
+ADE7816_SPI::ADE7816_SPI()
 {
-    _cs = pin;
+    spi = SPIClass();
+    settings();
 }
 
-int ADE7816_SPI::_writeRegister(const unsigned int reg, const unsigned int val)
+void ADE7816_SPI::begin(int8_t sck, int8_t miso, int8_t mosi, int8_t ss)
 {
-    // TODO: Implement using SPI mode transactions in Arduino
+    #ifdef ARDUINO_ARCH_ESP32
+    spi.begin(sck, miso, mosi, ss);
+    #else
+    spi.begin();
+    #endif
+    _ss = ss;
+    pinMode(_ss, OUTPUT);
+    digitalWrite(_ss, HIGH);
+    delay(1);
+    for (int i = 0; i < 4; i++)
+    {
+        digitalWrite(_ss, LOW);
+        delayMicroseconds(10);
+        digitalWrite(_ss, HIGH);
+        delayMicroseconds(10);
+    }
+}
+
+void ADE7816_SPI::settings(uint32_t clock, uint8_t bitOrder, uint8_t dataMode)
+{
+    _clock = clock;
+    _bitOrder = bitOrder;
+    _dataMode = dataMode;
+}
+
+void ADE7816_SPI::writeRegister(uint16_t reg, uint16_t val)
+{
+    uint8_t fba = 0x0;
+    uint8_t hba = reg >> 8;
+    uint8_t lba = reg & 0xff;
+    uint8_t hbv = val >> 8;
+    uint8_t lbv = val & 0xff;
+    digitalWrite(_ss, LOW);
+    spi.beginTransaction(SPISettings(_clock, _bitOrder, _dataMode));
+    spi.transfer(fba);
+    spi.transfer(hba);
+    spi.transfer(lba);
+    spi.transfer(hbv);
+    spi.transfer(lbv);
+    spi.endTransaction();
+    digitalWrite(_ss, HIGH);
+}
+
+uint32_t ADE7816_SPI::readRegister(uint16_t reg)
+{
+    uint8_t fba = 0x1;
+    uint8_t hba = reg >> 8;
+    uint8_t lba = reg & 0xff;
+    uint32_t res = 0;
+    digitalWrite(_ss, LOW);
+    spi.beginTransaction(SPISettings(_clock, _bitOrder, _dataMode));
+    spi.transfer(fba);
+    spi.transfer(hba);
+    spi.transfer(lba);
+    uint8_t rb0 = spi.transfer(0x0);
+    uint8_t rb1 = spi.transfer(0x0);
+    uint8_t rb2 = spi.transfer(0x0);
+    uint8_t rb3 = spi.transfer(0x0);
+    res |= rb0;
+    res = (res << 8) | rb1;
+    res = (res << 8) | rb2;
+    res = (res << 8) | rb3;
+    spi.endTransaction();
+    digitalWrite(_ss, HIGH);
+    return res;
+}
+
+void ADE7816_SPI::runDsp()
+{
+    ADE7816_SPI::writeRegister(Run, 1);
+}
+void ADE7816_SPI::stopDsp()
+{
+    writeRegister(Run, 0);
+}
+
+int ADE7816_SPI::_writeRegister(const unsigned int reg, const unsigned int val){
     return 0;
 }
-
-int ADE7816_SPI::_readRegister(const unsigned int reg)
-{
-    // TODO: Implement using SPI mode transactions in Arduino
+int ADE7816_SPI::_readRegister(const unsigned int reg){
     return 0;
 }
--- examples/ADE7816_SPI/ADE7816_SPI.ino
+++ examples/ADE7816_SPI/ADE7816_SPI.ino
@@ -2,13 +2,65 @@
 #include <SPI.h>
 #include "ADE7816.h"
 
+uint32_t vrms = 0;
+uint32_t ia_rms = 0;
+uint32_t ib_rms = 0;
+uint32_t ic_rms = 0;
+uint32_t id_rms = 0;
+uint32_t ie_rms = 0;
+uint32_t if_rms = 0;
+
 ADE7816_SPI eic = ADE7816_SPI();
 
 void setup()
 {
-  /* Initialize the serial port to host */
+
+  /* initialisation the serial port to host */
   Serial.begin(115200);
-  eic.init();
+  //initialisation with default spi pins
+  eic.begin();
+  //initialisation with specified spi pins (esp32 only)
+  //eic.begin(SCK, MISO, MOSI, SS);
+  eic.runDsp();
+  eic.runDsp();
+  eic.runDsp();
+  for (int i = 0; i < 10; i++)
+  {
+    vrms = eic.readRegister(VRMS);
+    Serial.print("VRMS=");
+    Serial.println(vrms);
+  }
+  for (int i = 0; i < 10; i++)
+  {
+    ia_rms = eic.readRegister(IARMS);
+    ib_rms = eic.readRegister(IBRMS);
+    ic_rms = eic.readRegister(ICRMS);
+    id_rms = eic.readRegister(IDRMS);
+    ie_rms = eic.readRegister(IERMS);
+    if_rms = eic.readRegister(IFRMS);
+    Serial.print("IRMS ");
+    Serial.print(ia_rms);
+    Serial.print(" ");
+    Serial.print(ib_rms);
+    Serial.print(" ");
+    Serial.print(ic_rms);
+    Serial.print(" ");
+    Serial.print(id_rms);
+    Serial.print(" ");
+    Serial.print(ie_rms);
+    Serial.print(" ");
+    Serial.println(if_rms);
+  }
+  eic.stopDsp();
+  eic.stopDsp();
+  eic.stopDsp();
+  for (int i = 0; i < 10; i++)
+  {
+    vrms = eic.readRegister(VRMS);
+    Serial.print("VRMS=");
+    Serial.println(vrms);
+  }
+
 }
 
 void loop()
